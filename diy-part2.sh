c4209f3ee87b7458c3e2d4178524e44340f998cd
dwa237d22 <106516436+dwa237d22@users.noreply.github.com>
2023-12-02 11:38:14 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,26 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修复部分插件自启动脚本丢失可执行权限问题
+sed -i '/exit 0/i\chmod +x /etc/init.d/*' package/lean/default-settings/files/zzz-default-settings
+
+
+
+# 删除重复包
+
+# rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf package/small-package/luci-app-openvpn-server
+rm -rf package/small-package/openvpn-easy-rsa-whisky
+rm -rf package/small-package/luci-app-wrtbwmon
+rm -rf package/small-package/wrtbwmon
+rm -rf package/small-package/luci-app-koolproxyR
+rm -rf package/small-package/luci-app-godproxy
+rm -rf package/small-package/luci-app-argon*
+rm -rf package/small-package/luci-theme-argon*
+rm -rf package/small-package/luci-app-amlogic
+rm -rf package/small-package/luci-app-unblockneteasemusic
+rm -rf package/small-package/upx-static
+rm -rf package/small-package/upx
+rm -rf package/small-package/firewall*
+rm -rf package/small-package/opkg
